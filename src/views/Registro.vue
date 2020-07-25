e1da1c0551b1447de032c902291c0339d82662a9
martin_dev <gmsuruguay@gmail.com>
2020-07-25 17:03:07 -0300

    se creo metodo para registrar usuario

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Registro de Usuarios</h1>
-        <form>
+        <form @submit.prevent="crearUsuario({email: email, password: password})">
             <input type="email" placeholder="Ingrese email" v-model="email">
             <input type="password" placeholder="Ingrese password" v-model="password">
             <input type="password" placeholder="Repetir password" v-model="retypePassword">
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
     export default {
         name:'Registro',
         data(){
@@ -19,6 +20,9 @@
                 password:'',
                 retypePassword:'' 
             }
+        },
+        methods:{
+            ...mapActions(['crearUsuario'])
         }
         
     }
