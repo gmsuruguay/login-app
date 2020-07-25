e027dda72adf8f8a6cedff936f3e8b78c17eca17
martin_dev <gmsuruguay@gmail.com>
2020-07-25 16:09:34 -0300

    se agrego formulario a la vista registro

---
+++ src/views/Registro.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Registro de Usuarios</h1>
+        <form>
+            <input type="email" placeholder="Ingrese email" v-model="email">
+            <input type="password" placeholder="Ingrese password" v-model="password">
+            <input type="password" placeholder="Repetir password" v-model="retypePassword">
+            <button type="submit">Registrar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Registro',
+        data(){
+            return {
+                email:'',
+                password:'',
+                retypePassword:'' 
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
